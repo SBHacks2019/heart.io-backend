3ca9d993ae5abc5e0028514161f6850744f8b0ab
Tejas Shah <tejashah88@gmail.com>
2019-01-11 23:03:34 -0800

    support for dynamic model loading and more flexible file handling

---
+++ src/errors/InvalidUsage.py
@@ -0,0 +1,16 @@
+# shamelessly copied from Flask docs
+# source: http://flask.pocoo.org/docs/1.0/patterns/apierrors/
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['error'] = self.message
+        return rv
--- src/server.py
+++ src/server.py
@@ -1,58 +1,100 @@
 import os.path
+from glob import glob
 
 import flask
 from flask import Flask, jsonify, request
-
 from flask_cors import CORS
 
 # from werkzeug.contrib.cache import FileSystemCache
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/input_files'
+UPLOAD_FOLDER = '../uploads'
+MODELS_FOLDER = '../models/*.pkl'
 ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg' ])
-MODEL_FILEPATH = 'model/model.pkl'
+DEFAULT_FILE_KEY = 'input'
 
 app = Flask(__name__)
 CORS(app)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from sklearn.externals import joblib
 
-model = None
-if os.path.exists(MODEL_FILEPATH):
-    model = joblib.load(MODEL_FILEPATH)
+from errors.InvalidUsage import InvalidUsage
+
+loaded_models = {}
 
 allowed_file = lambda filename: '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-throw_error = lambda msg: jsonify({ 'error': msg })
 res_success = lambda msg: jsonify({ 'success': msg })
+
+# custom methods
+
+def save_and_get_input_file(files_dict):
+    if DEFAULT_FILE_KEY not in files_dict:
+        raise InvalidUsage('No file detected!')
+
+    file = request.files[DEFAULT_FILE_KEY]
+
+    if file.filename == '':
+        raise InvalidUsage('No file selected!')
+    elif file and allowed_file(file.filename):
+        # construct the file name and path
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+        filedir = os.path.dirname(filepath)
+
+        # create the necessary directories (if applicable) and save the image
+        if not os.path.exists(filedir):
+            os.makedirs(filedir)
+        file.save(filepath)
+        return filepath
+    else:
+        raise InvalidUsage('Invalid file provided!')
+
+def predict_with_model(image_path, model_name):
+    model = loaded_models.get(model_name)
+    print(image_path)
+    if model is not None:
+        # TODO: implement classification
+        return res_success('We got the image!')
+    else:
+        # this shouldn't happen at all
+        raise InvalidUsage('The model file is not present!')
+
+# Flask methods
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
-    return jsonify({ 'message': 'hello world' })
+    return res_success('hello world')
 
-# define a predict function as an endpoint
-@app.route('/predict', methods=['POST'])
-def predict():
-    if 'file' not in request.files:
-        return throw_error('No file detected!')
+@app.route('/predict-mole', methods=['POST'])
+def predict_mole():
+    image_path = save_and_get_input_file(request.files)
+    results = predict_with_model(image_path, 'mole-model')
+    return results
 
-    file = request.files['file']
+@app.route('/predict-chest', methods=['POST'])
+def predict_chest():
+    image_path = save_and_get_input_file(request.files)
+    results = predict_with_model(image_path, 'chest-model')
+    return results
 
-    if file.filename == '':
-        return throw_error('No file selected!')
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-        if model is not None:
-            # TODO: implement classification
-            return res_success('We got the image!')
-        else:
-            return throw_error('The model file is not present!')
-
-# start the flask app, allow remote connections
 if __name__ == "__main__":
+    # load all models into memory
+    print('Loading models...')
+    for model_path in glob(MODELS_FOLDER):
+        print(model_path)
+        model_name = os.path.basename(model_path)
+        model_name_pure, model_name_ext = os.path.splitext(model_name)
+        loaded_models[model_name_pure] = joblib.load(model_path)
+    print(f'{len(loaded_models)} models loaded.')
+    print(loaded_models)
+
     app.run(
         host='0.0.0.0',
         port=8080,
